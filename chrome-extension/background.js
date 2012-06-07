8a4636762fc612ea131f819afecfff68400dacd5
Alex Sokolov <alex.sokols@gmail.com>
2012-06-07 11:42:33 +0300

    Initial edit mode commit

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -25,6 +25,72 @@ function loadData(file) {
     request.send(null);
 }
 
+function errorHandler(e) {
+  var msg = '';
+
+  switch (e.code) {
+    case FileError.QUOTA_EXCEEDED_ERR:
+      msg = 'QUOTA_EXCEEDED_ERR';
+      break;
+    case FileError.NOT_FOUND_ERR:
+      msg = 'NOT_FOUND_ERR';
+      break;
+    case FileError.SECURITY_ERR:
+      msg = 'SECURITY_ERR';
+      break;
+    case FileError.INVALID_MODIFICATION_ERR:
+      msg = 'INVALID_MODIFICATION_ERR';
+      break;
+    case FileError.INVALID_STATE_ERR:
+      msg = 'INVALID_STATE_ERR';
+      break;
+    default:
+      msg = 'Unknown Error';
+      break;
+  };
+
+  console.log('Error: ' + msg);
+}
+
+function onInitFs(fs) {
+		console.log("File system initialized");
+		  fs.root.getFile('log.txt', {create: true}, function(fileEntry) {
+
+			// Create a FileWriter object for our FileEntry (log.txt).
+			fileEntry.createWriter(function(fileWriter) {
+
+			  fileWriter.onwriteend = function(e) {
+				console.log('Write completed.');
+			  };
+
+			  fileWriter.onerror = function(e) {
+				console.log('Write failed: ' + e.toString());
+			  };
+
+			  // Create a new Blob and write it to log.txt.
+			  var bb = new window.WebKitBlobBuilder(); // Note: window.WebKitBlobBuilder in Chrome 12.
+			  bb.append('Lorem Ipsum');
+			  fileWriter.write(bb.getBlob('text/plain'));
+
+			}, errorHandler);
+
+		  }, errorHandler);
+}
+
+// save file to disk
+function saveFileToDisk(fileName, fileData){
+	  console.log("Writing file to disk");
+	  
+	 // Note: The file system has been prefixed as of Google Chrome 12:
+	window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+ 
+	 window.webkitStorageInfo.requestQuota(PERSISTENT, 1024*1024, function(grantedBytes) {
+	  window.requestFileSystem(PERSISTENT, grantedBytes, onInitFs, errorHandler);
+	}, function(e) {
+	  console.log('Error', e);
+	});	  	
+}
+
 // add time code to data
 function addTimeCode(title, start, end, code){
 	pastebin_codes = data[title];
@@ -38,6 +104,7 @@ function addTimeCode(title, start, end, code){
 	pastebin_codes.push(value);
 	data[title] = pastebin_codes;
 	console.log("Data after add : \n" + JSON.stringify(data));
+	saveFileToDisk("pastebin.txt", JSON.stringify(data));
 }
 
 
